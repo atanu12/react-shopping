c61560441c414638822b36ce700401db10f2cfa8
Atanu Mondal <you@example.com>
2021-01-13 20:31:16 +0530

    Created a cart page to display the cart functionality

---
+++ src/components/Carts.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    Container,
+    ListGroup,
+    ListGroupItem,
+    Button,
+    CardHeader,
+    CardBody,
+    Card,
+    CardFooter,
+    Row,
+    Col
+} from 'reactstrap'
+
+const Carts =()=> {
+    return (
+        
+    )
+}
+
+export default Carts
