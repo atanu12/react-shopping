f4a1bd5ea85794e947fe006edc2d7972c3ac3167
Atanu Mondal <you@example.com>
2021-01-13 20:25:27 +0530

    Remove border from the button

--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from "react";
 import {
-    Card, CardImg,CardText,CardBody,CardTitle, Button
-} from 'reactstrap'
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button,
+  Container,
+} from "reactstrap";
 
-const CardItems =({product, addInCart} )=> {
-    return (
-      <Card className='mt-2 mb-1' >
-        
-            <CardImg top height='250' width='100%' src={product.smallImage} />
-            <CardBody className='text-center' >
-                <CardTitle> {product.productName} </CardTitle>
-                <CardText className="error" >
-                    Price : Rs {product.productPrice}
-                </CardText>
-                <Button className='warning' onClick={ ()=> addInCart(product) } >Buy Now</Button>
-            </CardBody>
-
+const CardItems = ({ product, addInCart }) => {
+  return (
+    <Container>
+      <Card className="mt-2 mb-1">
+        <CardImg top height="250" width="100px" src={product.smallImage} />
+        <CardBody className="text-center">
+          <CardTitle> {product.productName} </CardTitle>
+          <CardText className="error">
+            Price : Rs {product.productPrice}
+          </CardText>
+          <Button className="warning border-0 " style={{outline: 'none'}} onClick={() => addInCart(product)}>
+            Buy Now
+          </Button>
+        </CardBody>
       </Card>
-    )
-}
+    </Container>
+  );
+};
 
-export default CardItems
+export default CardItems;
