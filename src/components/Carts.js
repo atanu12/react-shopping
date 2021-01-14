291e67f5fb00be93069be0d58bc1559d5d3d0623
Atanu Mondal <you@example.com>
2021-01-14 18:12:17 +0530

    created carts function got buy users and show the totail price

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -12,8 +12,53 @@ import {
     Col
 } from 'reactstrap'
 
-const Carts =()=> {
+const Carts =( {cartItem, buy,removeItem} )=> {
+
+    let amount = 0;
+    cartItem.forEach(item =>{
+        amount = parseFloat(amount) + parseFloat(item.productPrice)
+    })
     return (
+        <Container>
+            <h1 className="text-success"> Your Cart </h1>
+            <ListGroup>
+                {cartItem.map( item=>(
+                    <ListGroupItem key={item.id} >
+                        <Row>
+                            <Col>
+                                <img height={80} src={item.tinyImage} />
+                            </Col>
+                            <Col className='text-center' >
+                                <div className="text-primary"> {item.productname} </div>
+                                <span> Price :- {item.productPrice} </span>
+                                <Button color='danger' onClick={ ()=> removeItem(item) } >Remove</Button>
+                            </Col>
+
+                        </Row>
+                    </ListGroupItem>
+                    
+                ) )}
+            </ListGroup>
+            {/* if the cart is emptyCells:*/ }
+            {
+                cartItem.length>=1 ? (
+                    <Card className="text-center mt-3">
+                        <CardHeder >
+                            Toatal 
+                        </CardHeder>
+                        <CardBody>
+                            Your Amount for {cartItem.length} product is {amount} 
+                        </CardBody>
+                        <CardFooter>
+                            <Button color='success' onClick={buy} >Pay Here</Button>
+                        </CardFooter>
+                    </Card>
+                ) : (
+                    <h1 className="text-warning"> Cart is Empty </h1>
+                )
+            }
+
+        </Container>
         
     )
 }
